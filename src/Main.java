c3ac7f01f33443058675c023ba2d0ff020dba62b
Maya Ben zvi <maya.benzvi@ridewithvia.com>
2021-07-12 17:28:55 +0300

    added multiply method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         add(2, 3);
+        multiply(5, 6);
+    }
+
+    private static int multiply(int i, int i1) {
+        return i*i1;
     }
 
     private static int add(int i, int i1) {
